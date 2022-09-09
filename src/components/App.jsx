4a61e0265d894b89556bd6a06be60ee48db53ed1
ANDRII HALYTSKYI <galickyav@gmail.com>
2022-09-09 16:38:29 +0300

    02-15

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,71 @@
-import { Component } from 'react';
-import FeedbackOptions from './Feedback';
-import Statistics from './Statistics';
+import React, { Component } from 'react';
+import ContactsList from './contactsList/ContactsList';
+import ContactForm from './Form/Form';
+import Filter from './Filter/Filter';
+import { Title, ContactsListTitle } from './Phonebook.styled';
 
-
-export class App extends Component {
-
+class App extends Component {
   state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
 
-  
+  searchByName = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  addContact = newContact => {
+    this.setState(prevState => {
+      let isContains = false;
+      let updateContacts = [];
+      prevState.contacts.forEach(({ name }) => {
+        if (name === newContact.name) {
+          alert(`${name} is already in contacts`);
+          isContains = true;
+        }
+      });
+      isContains
+        ? (updateContacts = [...prevState.contacts])
+        : (updateContacts = [...prevState.contacts, newContact]);
+      return {
+        contacts: updateContacts,
+        name: '',
+        number: '',
+      };
+    });
+  };
+
+  deleteContact = e => {
+    const id = e.currentTarget.dataset.id;
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
   render() {
+    const { contacts, filter } = this.state;
+    const visibleContacts = contacts.filter(({ name }) =>
+      name.toLowerCase().includes(filter.toLowerCase())
+    );
+
     return (
       <>
-      <section title="Please leave feedback">
-          <FeedbackOptions
-          options = {this.state}
-          // onLeaveFeedback={this.handleChange}
+        <Title>Phonebook</Title>
+        <ContactForm addContact={this.addContact} />
+        <ContactsListTitle>Contacts</ContactsListTitle>
+        <Filter searchByName={this.searchByName} />
+        <ContactsList
+          phoneList={visibleContacts}
+          deleteContact={this.deleteContact}
         />
-        </section>
-        
-        <section title="Statistics">
-          <Statistics/>
-        </section>
-</>
+      </>
     );
-  };
-};
+  }
+}
+
+export default App;
